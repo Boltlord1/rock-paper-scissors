a078ab1490fb0a335d3d4b36fab3f5a5552e90c4
Boltlord <80578277+Boltlord1@users.noreply.github.com>
2025-08-15 14:25:39 +0500

    created round play function

--- javascript.js
+++ javascript.js
@@ -9,10 +9,26 @@ function moveID(move) {
 }
 
 function computerChoice() {
-    return moveID(Math.floor(Math.random() * 3))
+    return Math.floor(Math.random() * 3)
 }
 
 function playerChoice(choice) {
     choice = parseInt(prompt(`Enter your choice`, `0, 1 or 2`))
-    return moveID(choice)
+    return choice
+}
+
+function playRound(computer, player) {
+    let computerWon = `Computer wins, ${moveID(computer)} beats ${moveID(player)}`
+    let playerWon = `Player wins, ${moveID(player)} beats ${moveID(computer)}`
+    console.log(computer)
+    console.log(player)
+    if (computer === player) {
+        return `Draw, both sides chose ${moveID(computer)}`
+    } else if (computer === (player - 1)) {
+        return playerWon
+    } else if (computer === (player + 1) || computer === 0) {
+        return computerWon
+    } else {
+        return playerWon
+    }
 }
