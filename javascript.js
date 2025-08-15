57a01fc6a5bac239dec672e0ee5a8c0c5710516c
Boltlord <80578277+Boltlord1@users.noreply.github.com>
2025-08-15 14:41:11 +0500

    finished core game functionality

--- javascript.js
+++ javascript.js
@@ -17,18 +17,30 @@ function playerChoice(choice) {
     return choice
 }
 
-function playRound(computer, player) {
-    let computerWon = `Computer wins, ${moveID(computer)} beats ${moveID(player)}`
-    let playerWon = `Player wins, ${moveID(player)} beats ${moveID(computer)}`
-    console.log(computer)
-    console.log(player)
-    if (computer === player) {
-        return `Draw, both sides chose ${moveID(computer)}`
-    } else if (computer === (player - 1)) {
-        return playerWon
-    } else if (computer === (player + 1) || computer === 0) {
-        return computerWon
-    } else {
-        return playerWon
+function playGame() {
+    let playerScore = 0
+    let computerScore = 0
+    function playRound(computer, player) {
+        let computerWon = `Computer wins, ${moveID(computer)} beats ${moveID(player)}`
+        let playerWon = `Player wins, ${moveID(player)} beats ${moveID(computer)}`
+        if (computer === player) {
+            console.log(`Draw, both sides chose ${moveID(computer)}`)
+            return
+        } else if (computer === (player - 1)) {
+            console.log(playerWon)
+            return playerScore++
+        } else if (computer === (player + 1) || computer === 0) {
+            console.log(computerWon)
+            return computerScore++
+        } else {
+            console.log(playerWon)
+            return playerScore++
+        }
+    }
+    for (;(playerScore < 5 && computerScore < 5);) {
+        playRound(computerChoice(), playerChoice())
+        console.log(`Player ${playerScore} - ${computerScore} Computer`)
     }
 }
+
+playGame()
