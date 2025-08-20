cde857e20a16c6753ada7011a029b3235368a61f
Boltlord <80578277+Boltlord1@users.noreply.github.com>
2025-08-20 07:30:45 +0500

    added buttons in html and made them functional

--- javascript.js
+++ javascript.js
@@ -12,35 +12,28 @@ function computerChoice() {
     return Math.floor(Math.random() * 3)
 }
 
-function playerChoice(choice) {
-    choice = parseInt(prompt(`Enter your choice`, `0, 1 or 2`))
-    return choice
+function playRound(computerChoice, playerChoice) {
+    console.log(computerChoice + ` ` + playerChoice)
 }
 
-function playGame() {
-    let playerScore = 0
-    let computerScore = 0
-    function playRound(computer, player) {
-        let computerWon = `Computer wins, ${moveID(computer)} beats ${moveID(player)}`
-        let playerWon = `Player wins, ${moveID(player)} beats ${moveID(computer)}`
-        if (computer === player) {
-            console.log(`Draw, both sides chose ${moveID(computer)}`)
-            return
-        } else if (computer === (player - 1)) {
-            console.log(playerWon)
-            return playerScore++
-        } else if (computer === (player + 1) || computer === 0) {
-            console.log(computerWon)
-            return computerScore++
-        } else {
-            console.log(playerWon)
-            return playerScore++
-        }
+const buttons = document.querySelector(`.buttons`)
+buttons.addEventListener(`click`, (event) => {
+    let target = event.target
+    switch(target.id) {
+        case `rock`:
+            playRound(computerChoice(), 0)
+            break
+        case `paper`:
+            playRound(computerChoice(), 1)
+            break
+        case `scissors`:
+            playRound(computerChoice(), 2)
+            break
+        case `reset`:
+            console.log(3)
+            break
     }
-    for (;(playerScore < 5 && computerScore < 5);) {
-        playRound(computerChoice(), playerChoice())
-        console.log(`Player ${playerScore} - ${computerScore} Computer`)
-    }
-}
+})
 
-playGame()
+const scoreList = document.querySelector(`.score`)
+const scoreListItem = document.createElement(`li`)
